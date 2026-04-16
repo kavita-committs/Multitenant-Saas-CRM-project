b05adf2b3ee2e249709f95e546f233143d72a8cd
Kavita Jagtap <kavitajagtap3992@gmail.com>
2026-04-16 12:52:32 +0530

    Updated db.js with env configuration and connection pooling

--- src/config/db.js
+++ src/config/db.js
@@ -13,16 +13,6 @@ const config = {
   }
 };
 
-//module.exports = config;
-
-// const config = {
-//   user: "sa",
-//   password: "admin1234",
-//   server: "localhost",
-//   database: "crm_db",
-//   port : 1440,
-//   options: { trustServerCertificate: true }
-// };
 
 const pool = new sql.ConnectionPool(config).connect();
 module.exports = { sql, pool };
